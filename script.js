9722d300e2e1dcbc7e366e4f6d1a306a241132cb
Shem Bar <shem@svcollege.co.il>
2022-05-03 17:19:27 +0300

    g

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('hello world');
+const express = require('express')
+const app = express()
+
+
+app.get('/',(req,res)=>{
+    res.send('hello')
+})
+
+app.listen('8080',()=>{console.log('server listen to port 8080');})
